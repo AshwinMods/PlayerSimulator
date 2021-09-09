37be09be39d66ddfaeb51d984ac011f251930499
AshwinMods <ashwin.mods@gmail.com>
2021-09-09 22:47:06 +0530

    VS Project and Basic Classes

---
+++ PlrSim/GameConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlrSim
+{
+	class GameConfig
+	{
+		#region Variable Declarations
+		string m_GameName;
+		List<PatternRect> m_Patterns;
+		List<ActionSequence> m_ActionSequence;
+		#endregion
+
+		#region Constructors
+		public GameConfig(string path)
+		{
+
+		}
+		#endregion
+	}
+}
